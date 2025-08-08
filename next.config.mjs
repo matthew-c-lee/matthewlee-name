37513a24399ebec9c56da0da69dd5e82ef959024
Matthew Lee <mail@matthewlee.name>
2025-08-08 14:28:59 -0400

    mdx + travel guide

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    basePath: "",
-    assetPrefix: "",
-};
+// next.config.mjs
+import createMDX from '@next/mdx';
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+});
